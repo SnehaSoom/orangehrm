0ea2cfb1e05ed559abe393014269039303b83be4
ashishMaurya <>
2024-04-14 18:56:15 +0530

    add

--- src/App/Routes.js
+++ src/App/Routes.js
@@ -21,6 +21,14 @@ export default function Routers() {
                         <Route path="/search" element={<LoginIndex />} />
                         <Route path="/admin" element={<LoginIndex />} />
                         <Route path="/pim" element={<PIMIndex />} />
+                        <Route path="/leave" element={<h1>leave</h1>} />
+                        <Route path="/time" element={<h1>time</h1>} />
+                        <Route path="/dashboard" element={<h1>dashboard</h1>} />
+                        <Route path="/directory" element={<h1>directory</h1>} />
+                        <Route path="/maintenance" element={<h1>maintenance</h1>} />
+                        <Route path="/my-info" element={<h1>My Info</h1>} />
+                        <Route path="/buzz" element={<h1>buzz</h1>} />
+                        
                     </Route>
                 </Routes>
             </BrowserRouter>
--- src/components/Layout/SideBar/SideBar.js
+++ src/components/Layout/SideBar/SideBar.js
@@ -1,27 +1,29 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Search, AdminIcon, PimIcon,LeftIcon } from "./../../../App/Assests";
+import { Search, AdminIcon, PimIcon, LeftIcon } from "./../../../App/Assests";
+import { useLocation } from 'react-router-dom';
 import "./SideBar.css";
 
 const menuItems = [
-  { text: "Search", icon: <Search color="gray" size={25} />, link: "/search" },
-  { text: "Admin", icon: <AdminIcon color="gray" size={25} />, link: "/admin" },
-  { text: "PIM", icon: <PimIcon color="gray" size={25} />, link: "/pim" },
-  { text: "Leave", icon: <AdminIcon color="gray" size={25} />, link: "/leave" }, // Example link for "Leave"
-  { text: "Time", icon: <AdminIcon color="gray" size={25} />, link: "/time" }, // Example link for "Time"
-  { text: "My Info", icon: <AdminIcon color="gray" size={25} />, link: "/my-info" }, // Example link for "My Info"
-  { text: "Dashboard", icon: <AdminIcon color="gray" size={25} />, link: "/dashboard" }, // Example link for "Dashboard"
-  { text: "Directory", icon: <AdminIcon color="gray" size={25} />, link: "/directory" }, // Example link for "Directory"
-  { text: "Maintenance", icon: <AdminIcon color="gray" size={25} />, link: "/maintenance" }, // Example link for "Maintenance"
-  { text: "Buzz", icon: <AdminIcon color="gray" size={25} />, link: "/buzz" }, // Example link for "Buzz"
+  { text: "Search", icon: <Search color="inherit" size={25} />, link: "/search" },
+  { text: "Admin", icon: <AdminIcon color="inherit" size={25} />, link: "/admin" },
+  { text: "PIM", icon: <PimIcon color="inherit" size={25} />, link: "/pim" },
+  { text: "Leave", icon: <AdminIcon color="inherit" size={25} />, link: "/leave" }, // Example link for "Leave"
+  { text: "Time", icon: <AdminIcon color="inherit" size={25} />, link: "/time" }, // Example link for "Time"
+  { text: "My Info", icon: <AdminIcon color="inherit" size={25} />, link: "/my-info" }, // Example link for "My Info"
+  { text: "Dashboard", icon: <AdminIcon color="inherit" size={25} />, link: "/dashboard" }, // Example link for "Dashboard"
+  { text: "Directory", icon: <AdminIcon color="inherit" size={25} />, link: "/directory" }, // Example link for "Directory"
+  { text: "Maintenance", icon: <AdminIcon color="inherit" size={25} />, link: "/maintenance" }, // Example link for "Maintenance"
+  { text: "Buzz", icon: <AdminIcon color="inherit" size={25} />, link: "/buzz" }, // Example link for "Buzz"
 ];
 
 const SideBar = ({
-  activeIndex,
   onClick,
   onClose,
 }) => {
   const navigate = useNavigate();
+  const location = useLocation();
+  const activeUrl = location.pathname;
   return (
     <div className="sidebarContainer">
       <div className="sidebar">
@@ -31,11 +33,11 @@ const SideBar = ({
         <div className="sidebarMenu">
           <ul className="menu-items">
             {menuItems.map((item, index) => (
-              <li className={activeIndex === index ? "menu-item-active" : "menu-item"} key={index} onClick={() => {
+              <li className={activeUrl === item.link ? "menu-item-active" : "menu-item"} key={index} onClick={() => {
                 onClick(index);
                 navigate(item.link)
               }}>
-                <span className={`icon`}>
+                <span className={activeUrl === item.link ? 'selected-icon' : 'icon'}>
                   {item.icon}
                 </span>
                 <span className="text">{item.text}</span>
